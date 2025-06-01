d21463de77d66821ff0dc79ae7a80410b78d6267
Lukas Frey <lukas.frey03@outlook.de>
2025-06-01 23:27:09 +0200

    get rid of libsql

--- db/db.ts
+++ db/db.ts
@@ -1,14 +1,10 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 import * as schema from "./schema";
 import { env } from "~/env";
 
 export const db = drizzle(
-  createClient({
-    url: env.LOCAL_SQLITE_PATH
-      ? `file://${env.LOCAL_SQLITE_PATH}`
-      : "file:db/sqlite.db",
-  }),
+  new Database(env.LOCAL_SQLITE_PATH ?? "db/sqlite.db"),
   {
     schema,
   },
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,7 @@
 import type { Config } from "drizzle-kit";
-import { env } from "./env";
 
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "libsql",
-  dbCredentials: {
-    url: env.LOCAL_SQLITE_PATH
-      ? `file://${env.LOCAL_SQLITE_PATH}`
-      : "file:db/sqlite.db",
-  },
+  dialect: "sqlite",
 } satisfies Config;
